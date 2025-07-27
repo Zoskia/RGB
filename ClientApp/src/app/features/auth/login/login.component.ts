b087bef54922ae539bd94adac226505471a3ff8f
Michael Varga <vargamichael@hotmail.com>
2025-07-27 15:03:09 +0200

    login frontend implemented

--- ClientApp/src/app/features/auth/login/login.component.ts
+++ ClientApp/src/app/features/auth/login/login.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { LoginService } from './login.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginUserDto } from '../../../dtos/login-user.dto';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private loginService: LoginService) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (!this.loginForm.valid) {
+      console.log('Form data invalid!');
+    }
+
+    const formData = this.loginForm.value;
+    const loginUser: LoginUserDto = {
+      username: formData.username,
+      password: formData.password,
+    };
+
+    this.loginService.loginUser(loginUser).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response);
+      },
+      error: (err) => {
+        console.error('Login failed:', err);
+      },
+    });
+  }
+}
--- ClientApp/src/app/features/auth/login/login.service.ts
+++ ClientApp/src/app/features/auth/login/login.service.ts
@@ -1,19 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { RegisterUserDto } from '../../../dtos/register-user.dto';
+import { LoginUserDto } from '../../../dtos/login-user.dto';
 import { UserResponseDto } from '../../../dtos/user-response.dto';
+import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RegisterService {
+export class LoginService {
   constructor(private http: HttpClient) {}
 
-  registerUser(user: RegisterUserDto): Observable<UserResponseDto> {
+  loginUser(user: LoginUserDto): Observable<UserResponseDto> {
     return this.http.post<UserResponseDto>(
-      `${environment.apiUrl}/user/register`,
+      `${environment.apiUrl}/user/login`,
       user
     );
   }
